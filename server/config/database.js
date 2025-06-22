b46fdfe23b13e828229b23a818de09d77f15303d
adeeb-github <112182422+adeeb-github@users.noreply.github.com>
2025-06-22 22:22:32 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -5,7 +5,7 @@ const { MONGODB_URL } = process.env;
 
 exports.connect = () => {
 	mongoose
-		.connect("mongodb://localhost:27017/majorproject", {
+		.connect("mongodb+srv://mohdadeeb409:LsHSjDUwGzaVGLfJ@cluster0.b8jjypu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
 			useNewUrlparser: true,
 			useUnifiedTopology: true,
 		})
